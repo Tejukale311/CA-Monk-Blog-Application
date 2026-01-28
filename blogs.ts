7a95922d78de43294b94bb020f8fef4c3add9c46
Tejasvi Kale <tejukale706@gmail.com>
2026-01-28 16:59:44 +0530

    Add files via upload

---
+++ blogs.ts
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+
+const API_URL = "http://localhost:3001/blogs";
+
+export const getBlogs = async (): Promise<Blog[]> => {
+  const res = await axios.get(API_URL);
+  return res.data;
+};
+
+export const getBlogById = async (id: number): Promise<Blog> => {
+  const res = await axios.get(`${API_URL}/${id}`);
+  return res.data;
+};
+
+export const createBlog = async (
+  blog: Omit<Blog, "id">
+): Promise<Blog> => {
+  const res = await axios.post(API_URL, blog);
+  return res.data;
+};
